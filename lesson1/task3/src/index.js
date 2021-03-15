a30cf7f73059035e55aeda8d6e00c64012a05c84
gon4aruk <sashagonch27@gmail.com>
2021-03-15 13:26:28 +0200

    first app jsx

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -1,26 +1,6 @@
-// const renderGreeting = elem => {
-//   const containerElem = document.createElement('div');
-//   containerElem.classList.add('greeting');
-
-//   const titleElem = document.createElement('h1');
-//   titleElem.classList.add('greeting__title');
-//   titleElem.textContent = 'Hello, world!';
-
-//   const textElem = document.createElement('p');
-//   textElem.classList.add('greeting__text');
-//   textElem.textContent = "I'm learning React";
-
-//   containerElem.append(titleElem, textElem);
-
-//   elem.append(containerElem);
-// };
-
-// renderGreeting(rootElement);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
-
 
 const rootElement = document.querySelector('#root');
 
