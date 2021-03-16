b525b18892656aa5d9cd7f2e15356eeb2652d59a
gon4aruk <sashagonch27@gmail.com>
2021-03-16 10:34:03 +0200

    jsx композиция компонентов

--- lesson3/task2/src/index.jsx
+++ lesson3/task2/src/index.jsx
@@ -11,4 +11,4 @@ const userInfo = {
     'https://avatars.githubusercontent.com/u/68783546?s=460&u=ec83ac56b1793cc8f60f36bde15ced45841ae26f&v=4',
 };
 
-ReactDOM.render(<Comment user={userInfo} text="Good job!" date={new Date()} />, rootElement);
+ReactDOM.render(<Comment author={userInfo} text="Good job!" date={new Date()} />, rootElement);
