87e448a780ebbe7536bb2cdf0a1ce5863b1c4e66
gon4aruk <sashagonch27@gmail.com>
2021-03-19 13:37:54 +0200

    dimension

--- lesson8/task3/src/index.jsx
+++ lesson8/task3/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Dimension from './Dimension';
+import Dimension from './Dimensions';
 
 const rootElement = document.querySelector('#root');
 
