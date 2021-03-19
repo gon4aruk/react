59385a298f7db289a2c1db40db56fce9eb075744
gon4aruk <sashagonch27@gmail.com>
2021-03-19 12:51:53 +0200

    мировые часы 2.0

---
+++ lesson8/task2/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Clock from './Clock';
+
+class App extends Component {
+  state = {
+    status: true,
+  };
+
+  clearClocks = () => {
+    this.setState({
+      status: !this.state.status,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        {this.state.status && (
+          <>
+            <Clock location="New York" offset={-5} />
+            <Clock location="Kyiv" offset={2} />
+            <Clock location="London" offset={0} />
+          </>
+        )}
+        <button onClick={this.clearClocks}>Toggle</button>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ lesson8/task2/src/Clock.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+const getTimeWithOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    const { location, offset } = props;
+
+    this.state = {
+      location,
+      time: getTimeWithOffset(offset),
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        time: getTimeWithOffset(this.props.offset),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <div className="clock__location">{this.state.location}</div>
+        <div className="clock__time">{moment(this.state.time).format('hh:mm:ss')}</div>
+      </div>
+    );
+  }
+}
+
+export default Clock;
