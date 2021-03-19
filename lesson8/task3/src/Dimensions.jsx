8daceb190174dcff346b9f83702bc605ce1e9465
gon4aruk <sashagonch27@gmail.com>
2021-03-19 13:35:02 +0200

    setDimensions

---
+++ lesson8/task3/src/Dimensions.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Dimensions extends Component {
+  state = {
+    width: null,
+    height: null,
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+
+    const { innerWidth, innerHeight } = window;
+    this.setDimensions(innerWidth, innerHeight);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = event => {
+    const { innerWidth, innerHeight } = event.target;
+    this.setDimensions(innerWidth, innerHeight);
+  };
+
+  setDimensions = (innerWidth, innerHeight) => {
+    this.setState({
+      width: innerWidth,
+      height: innerHeight,
+    });
+
+    document.title = `${innerWidth} x ${innerHeight}`;
+  };
+
+  render() {
+    const { width, height } = this.state;
+    return <div className="dimeensions">{`${width}px - ${height}px`}</div>;
+  }
+}
+
+export default Dimensions;
