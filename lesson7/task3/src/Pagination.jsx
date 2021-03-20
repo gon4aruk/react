ff2c9bd154bd39225e752a867dc2384b637b7b7e
gon4aruk <sashagonch27@gmail.com>
2021-03-20 12:15:44 +0200

    pagination

--- lesson7/task3/src/Pagination.jsx
+++ lesson7/task3/src/Pagination.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Pagination = ({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) => {
-  const isPrevPageAviable = currentPage !== 1 ? true : false;
-  const isNextPageAviable = currentPage !== Math.ceil(totalItems / itemsPerPage) ? true : false;
+class Pagination extends Component {
+  render() {
+    const { goPrev, goNext, currentPage, totalItems, itemsPerPage } = this.props;
+    const isPrevPageAviable = currentPage !== 1 ? true : false;
+    const isNextPageAviable = currentPage !== Math.ceil(totalItems / itemsPerPage) ? true : false;
 
-  const prevButton = isPrevPageAviable 
-    ? (<button className="btn" onClick={goPrev}>←</button>) 
-    : (<button className="btn" disabled={true}></button>);
+    const prevButton = isPrevPageAviable ? (
+      <button className="btn" onClick={goPrev}>
+        ←
+      </button>
+    ) : (
+      <button className="btn" disabled={true}></button>
+    );
 
-  const nextButton = isNextPageAviable 
-    ? (<button className="btn" onClick={goNext}>→</button>) 
-    : (<button className="btn" disabled={true}></button>);
-  
-  return (
-    <div className="pagination">
-      {prevButton}
-      <span className="pagination__page">{currentPage}</span>
-      {nextButton}
-    </div>
-  );
-};
+    const nextButton = isNextPageAviable ? (
+      <button className="btn" onClick={goNext}>
+        →
+      </button>
+    ) : (
+      <button className="btn" disabled={true}></button>
+    );
+
+    return (
+      <div className="pagination">
+        {prevButton}
+        <span className="pagination__page">{currentPage}</span>
+        {nextButton}
+      </div>
+    );
+  }
+}
 
 export default Pagination;
