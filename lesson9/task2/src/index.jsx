fe370899cad5a905d6639b16ba17f2280c363fc6
gon4aruk <sashagonch27@gmail.com>
2021-03-22 09:44:03 +0200

    uncontrolled form

--- lesson9/task2/src/index.jsx
+++ lesson9/task2/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Life from './Life';
+import App from './App';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Life />, rootElement);
+ReactDOM.render(<App />, rootElement);
