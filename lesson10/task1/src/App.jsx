71cfc91b4e6caccfd1954b9f5a2dbfbcc823df6f
gon4aruk <sashagonch27@gmail.com>
2021-03-23 10:51:52 +0200

    state lifting

---
+++ lesson10/task1/src/App.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import UserMenu from './UserMenu';
+import UserProfile from './UserProfile';
+
+class App extends Component {
+  state = {
+    userData: null,
+  };
+
+  componentDidMount() {
+    this.fetchUser(this.props.userName);
+  }
+
+  fetchUser = userName => {
+    fetch(`https://api.github.com/users/${userName}`)
+      .then(response => response.json())
+      .then(userData => {
+        this.setState({
+          userData,
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <header className="header">
+          <UserMenu userData={this.state.userData} />
+        </header>
+        <UserProfile userData={this.state.userData} />
+      </div>
+    );
+  }
+}
+
+export default App;
