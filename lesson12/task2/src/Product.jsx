89fe082c9528248caafea5ee2d81937f605e61e3
gon4aruk <sashagonch27@gmail.com>
2021-03-27 13:44:35 +0200

    react route

--- lesson12/task2/src/Product.jsx
+++ lesson12/task2/src/Product.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Product = ({ match }) => {
-  return <div className="product">{`Product is a ${match.params.productId}`}</div>;
+  return <div className="product">{match.params.productId}</div>;
 };
 
 export default Product;
