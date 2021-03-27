fdc1e23d22a283678db02ab638a7fa856bb4ab38
gon4aruk <sashagonch27@gmail.com>
2021-03-27 13:42:53 +0200

    react route

--- lesson12/task2/src/App.jsx
+++ lesson12/task2/src/App.jsx
@@ -1,31 +1,28 @@
-import React, { Component } from 'react';
-import Numbers from './Numbers';
-import OddNumbers from './OddNumbers';
-import EvenNumbers from './EvenNumbers';
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Products from './Products';
 
-class Page extends Component {
-  state = {
-    number: 0,
-  };
+const Page = () => {
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
 
-  componentDidMount() {
-    this.interval = setInterval(() => {
-      this.setState({
-        number: this.state.number + 1,
-      });
-    }, 1000);
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <OddNumbers number={this.state.number} />
-        <Numbers title="All numbers" number={this.state.number} />
-        <EvenNumbers number={this.state.number} />
-        <Numbers title="Just 17" number={17} />
-      </div>
-    );
-  }
-}
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products" component={Products} />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default Page;
