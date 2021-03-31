d64e846e2cc62e6820d34fde5bbdfe54aaa859ba
gon4aruk <sashagonch27@gmail.com>
2021-03-31 11:20:22 +0300

    online/offline status with hiiks

--- lesson13/task4/src/App.jsx
+++ lesson13/task4/src/App.jsx
@@ -1,29 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Users from './Users';
-import Home from './Home';
+import ConnectionStatus from './ConnectionStatus';
 
 const Page = () => {
-  return (
-    <div className="page">
-      <BrowserRouter>
-        <ul className="navigation">
-          <li className="navigation__item">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="navigation__item">
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-      </BrowserRouter>
-    </div>
-  );
+  return <ConnectionStatus />;
 };
 
 export default Page;
---
+++ lesson13/task4/src/ConnectionStatus.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+const ConnectionStatus = () => {
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    const handleOnline = () => {
+      setIsOnline(true);
+    };
+
+    const handleOffline = () => {
+      setIsOnline(false);
+    };
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
+
+  if (isOnline) {
+    return <div className="status">Online</div>;
+  }
+  return <div className="status status_offline">Offline</div>;
+};
+
+export default ConnectionStatus;
