92ca4edcedfebb45326cc1e31039ab2250c6bf3a
gon4aruk <sashagonch27@gmail.com>
2021-04-10 13:22:03 +0300

    redux actions/store

--- redux/lesson2/task1/src/index.js
+++ redux/lesson2/task1/src/index.js
@@ -9,9 +9,9 @@ export const decrement = {
 
 const counterRuducer = (state = 0, action) => {
   switch (action.type) {
-    case INCREMENT:
+    case 'COUNTER/INCREMENT':
       return state + 1;
-    case DECREMENT:
+    case 'COUNTER/DECREMENT':
       return state - 1;
     default:
       return state;
