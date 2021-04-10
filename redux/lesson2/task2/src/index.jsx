0ff30fcb0f829237f962c863c420999d4e8d5823
gon4aruk <sashagonch27@gmail.com>
2021-04-10 14:45:29 +0300

    redux/usersList

--- redux/lesson2/task2/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import Page from './App';
-
-const rootElement = document.querySelector('#root');
-
-ReactDOM.render(<Page />, rootElement);
