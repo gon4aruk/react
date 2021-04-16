695e65012ac3c38d7536ba335ab8afda96ce34bb
gon4aruk <sashagonch27@gmail.com>
2021-04-16 10:24:43 +0300

    redux async action

---
+++ redux/lesson6/task1/src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import { counterReducer } from './counter.reducer';
+
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+const store = createStore(counterReducer, applyMiddleware(logger));
+export default store;
